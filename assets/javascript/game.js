95dd010ef8538696db3bda89bb105d292f15d9bb
Jake Stenger <myselfjake@gmail.com>
2017-02-16 16:59:17 -0800

    starting to conceptualize the core functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,80 @@
-var players = {
+var player = "";
+var challenger = "";
+var vanquished = 0;
+var characters = {
 	picard: {
+		hp: 0, // hit-points
+		ap: 0, // attack-points
+		ca: 0, // counter-attack
+		vm: 10, // vulnerability multiplier
+		vul: "Force to interact with a child!",
+		status: ""
+	},
+	Q: {
+		hp: 100000,
+		ap: 0,
+		ca: 100000,
+		vm: 100000,
+		vul: "Ignore!",
+		status: ""
+	},
+	borg: {
 		hp: 0,
 		ap: 0,
 		ca: 0,
 		vm: 10,
+		vul: "Cycle phase variance!",
+		status: ""
 	},
+	Lore: {
+		hp: 0,
+		ap: 0,
+		ca: 0,
+		vm: 10,
+		vul: "Overload with emotion!",
+		status: ""
+	},
+	Klingon: {
+		hp: 0,
+		ap: 0,
+		ca: 0,
+		vm: 10,
+		vul: "Insult honor!",
+		status: ""
+	},
+	Romulan: {
+		hp: 0,
+		ap: 0,
+		ca: 0,
+		vm: 10,
+		vul: "Compare to a Vulcan",
+		status: ""
+	}
+
+}
+
+function characterStats() {
+	// This function generates stats for the active players
+
+}
+
+function randomCharacterAvailability() {
+	// This function chooses which characters are available to play
+}
+
+function changeCharacterStat(character, stat, newVal){
+	// This function just changes the obj's attribute
+	checkGameConditions()
+	return newStat;
+} 
+
+function checkGameConditions() {
+	// this function checks to see if the challenger has been defeated, if the
+	// player has been defeated, and if the game has been won/lost
 	
 }
+
+function displayStatInfoBox() {
+	// this function shows the character's vital stats on hover
+}
+
