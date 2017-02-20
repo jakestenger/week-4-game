3582ada4c1c7e1d426817614d8ca3ccf0fc21fdc
jakestenger <myselfjake@gmail.com>
2017-02-20 09:42:35 -0800

    save

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,6 @@
 var player = "";
 var challenger = "";
 var vanquished = 0;
-var position = [hidden, selection_bar, player, opponent]
 var characters = {
 	picard: {
 		hp: 0, // hit-points
@@ -9,8 +8,8 @@ var characters = {
 		ca: 0, // counter-attack
 		vm: 10, // vulnerability multiplier
 		vul: "Force to interact with a child!",
-		status: "".
-		position: ""
+		status: "",
+		img: "assets/images/picard.jpg"
 	},
 	Q: {
 		hp: 100000,
@@ -18,8 +17,8 @@ var characters = {
 		ca: 100000,
 		vm: 100000,
 		vul: "Ignore!",
-		status: "".
-		position: ""
+		status: "",
+		img: "assets/images/picard.jpg"
 	},
 	borg: {
 		hp: 0,
@@ -27,8 +26,9 @@ var characters = {
 		ca: 0,
 		vm: 10,
 		vul: "Cycle phase variance!",
-		status: "".
-		position: ""
+		status: "",
+		bg: 
+		img: "assets/images/picard.jpg"
 	},
 	Lore: {
 		hp: 0,
@@ -36,8 +36,8 @@ var characters = {
 		ca: 0,
 		vm: 10,
 		vul: "Overload with emotion!",
-		status: "".
-		position: ""
+		status: "",
+		img: "assets/images/picard.jpg"
 	},
 	Klingon: {
 		hp: 0,
@@ -45,8 +45,8 @@ var characters = {
 		ca: 0,
 		vm: 10,
 		vul: "Insult honor!",
-		status: "".
-		position: ""
+		status: "",
+		img: "assets/images/picard.jpg"
 	},
 	Romulan: {
 		hp: 0,
@@ -54,11 +54,21 @@ var characters = {
 		ca: 0,
 		vm: 10,
 		vul: "Compare to a Vulcan",
-		status: "".
-		position: ""
+		status: "",
+		img: "assets/images/picard.jpg"
 	}
 
 }
+
+$(".character").draggable();
+// $("#droppable").droppable({
+//  	drop: function(event, ui) {
+// 		$(this)
+// 			.addClass("ui-state-highlight")
+// 			.find("p")
+// 			.html("Dropped!");
+// 	}
+// });
 
 function characterStats() {
 	// This function generates stats for the active players
@@ -89,3 +99,26 @@ function attack(type) {
 
 }
 
+
+
+function putCharacterInDiv(char, pos) {
+	if (pos === "player") {
+		$("#sprite-left").prepend('<div class="character img-responsive"><img src="' + characters[char]["img"] + '" class="img" /></div>');
+	} else if (pos === "opponent") {
+		$("#bench").prepend('<div class="character img-responsive"><img src="' + characters[char]["img"] + '" class="img" /></div>');
+		$("#bg").css('background-image', characters[char]["bg"]).fadein(slow);
+	} else if (pos === "bench-1") {
+		$("#bench-1").prepend('<div class="character img-responsive"><img src="' + characters[char]["img"] + '" class="img" /></div>');
+	} else if (pos === "bench-2") {
+		$("#bench-2").prepend('<div class="character img-responsive"><img src="' + characters[char]["img"] + '" class="img" /></div>');
+	}  else if (pos === "bench-3") {
+		$("#bench-3").prepend('<div class="character img-responsive"><img src="' + characters[char]["img"] + '" class="img" /></div>');
+	}  else if (pos === "bench-4") {
+		$("#bench-4").prepend('<div class="character img-responsive"><img src="' + characters[char]["img"] + '" class="img" /></div>');
+	} else {
+		console.log("unexpected value");
+	}
+}
+
+
+putCharacterInDiv("picard", "bench-1");
